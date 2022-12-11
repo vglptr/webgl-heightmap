3656bbdc13649ead66a21065f3e67d42b8e01a00
Peter Vogel <vglptr@gmail.com>
2022-12-11 14:27:08 +0100

    Use vec2 in vertex shader

--- start.js
+++ start.js
@@ -13,7 +13,7 @@ void main() {
   vec2 zeroToOne = a_position / u_resolution;
   vec2 zeroToTwo = zeroToOne * 2.0;
   vec2 clipSpace = zeroToTwo - 1.0;
-  gl_Position = vec4(clipSpace, 0, 1);
+  gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
 }
 `;
 
